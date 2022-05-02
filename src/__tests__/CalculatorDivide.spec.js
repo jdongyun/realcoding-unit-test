989f055870ddf26d82a50aa39558fa5a7e92b1c8
Dongyun <dyunzero@gmail.com>
2022-05-02 22:52:38 +0900

    test: 0으로 나눴을 때 예외 발생 테스트 로직 추가

--- src/__tests__/CalculatorDivide.spec.js
+++ src/__tests__/CalculatorDivide.spec.js
@@ -1,4 +1,4 @@
-import Calculator from "../js/calculator/Calculator";
+import Calculator, { DivideZeroError } from "../js/calculator/Calculator";
 
 describe("사칙연산 나눗셈 테스트.", () => {
   test("4를 2로 나누면 몫이 2이다.", () => {
@@ -8,6 +8,6 @@ describe("사칙연산 나눗셈 테스트.", () => {
 
   test("0으로 나누면 에러가 발생한다.", () => {
     const calculator = new Calculator();
-    expect(calculator.divide(4, 0)).toEqual(Infinity);
+    expect(() => calculator.divide(4, 0)).toThrow(DivideZeroError);
   });
 });
