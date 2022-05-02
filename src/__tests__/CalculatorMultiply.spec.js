15829df1b690f7e151462189df3213d3bca82875
Dongyun Jeong <dongyun.jeong.15@gmail.com>
2022-05-02 16:48:02 +0900

    test: 곱셈 테스트 (#4)
    * test: 곱셈 테스트 코드 추가
    
    * test: 곱셈 테스트 코드 오타 수정
    
    Co-authored-by: dlwnsrb0829 <ijg0829@naver.com>

--- src/__tests__/CalculatorMultiply.spec.js
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -3,6 +3,6 @@ import Calculator from "../js/calculator/Calculator";
 describe("사칙연산 곱셈 테스트.", () => {
   test("3과 5를 곱하면 15이다.", () => {
     const calculator = new Calculator();
-    expect(calculator.mulitiply(3, 5)).toEqual(15);
+    expect(calculator.multiply(3, 5)).toEqual(15);
   });
 });
