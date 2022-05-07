0c94acef53c98d8288d41984fb3d368d5091ceab
Dongyun <dyunzero@gmail.com>
2022-05-07 19:27:23 +0900

    테스트 코드 수정

--- src/__tests__/201703112/__tests__/Calculator.spec.js
+++ src/__tests__/201703112/__tests__/Calculator.spec.js
@@ -1,3 +1,5 @@
+import Calculator from "../../../js/calculator/Calculator";
+
 beforeAll(() => {
   console.log("모든 테스트를 시작하기 전 한번만 실행된다.");
 });
@@ -20,25 +22,24 @@ expect.toBeFalsy
 expect.not
 expect.toBeInstanceOf
 */
+const calculator = new Calculator();
 
 describe("음수 양수 판단", () => {
-  const isNegative = (num) => num < 0;
-    test("isNegative에 7을 넣으면 false를 반환한다.", () => {
-      expect(isNegative(7)).toBeFalsy();
-    });
-    test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
-      expect(isNegative(-3)).toBeTruthy();
-    });
-    test("isNegative에 0을 넣으면 true가 아니다.", () => {
-      expect(isNegative(0)).not.toEqual(true);
-    });
+  test("isNegative에 7을 넣으면 false를 반환한다.", () => {
+    expect(calculator.isNegative(7)).toBeFalsy();
   });
-  describe("Date 클래스 판단하기", () => {
-    const getDate = () => new Date();
-    
-    test("getDate를 하면 Date객체가 나온다.", () => {
-        expect(getDate()).toBeInstanceOf(Date);
-    });
+  test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
+    expect(calculator.isNegative(-3)).toBeTruthy();
+  });
+  test("isNegative에 0을 넣으면 true가 아니다.", () => {
+    expect(calculator.isNegative(0)).not.toEqual(true);
+  });
+});
+
+describe("Date 클래스 판단하기", () => {
+  test("getDate를 하면 Date 객체가 나온다.", () => {
+    expect(calculator.getDate()).toBeInstanceOf(Date);
+  });
 });
 
 /*
@@ -48,25 +49,20 @@ expect.toHaveBeenCalled
 expect.toHaveBeenCalledWith
 */
 
-describe("가상 함수 테스트해보기", () => {
-    test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
-      const jestFn = jest.fn(() => 0);
-      const customCalculation = f => f();
-      customCalculation(jestFn);
-      expect(jestFn).toHaveBeenCalled();
-    });
-    test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
-      const jestFn = jest.fn((a, b, c) => a + b + c);
-      const customCalculation = (f, a, b, c) => f(a, b, c);
-      customCalculation(jestFn, 1, 2, 3);
-      expect(jestFn).toBeCalledWith(1, 2, 3);
-    });
-    test("abs함수는 Math.abs를 호출한다.", () => {
-      const jestSpyOn = jest.spyOn(Math, 'abs');
-      const abs = (num) => {
-        Math.abs(num);
-      }
-      abs(-1);
-      expect(jestSpyOn).toBeCalledTimes(1);
-    });
+describe("가상 함수 테스트 해보기", () => {
+  test("customCalculation은 전달된 함수를 호출시킨다.", () => {
+    const func = jest.fn(() => 0);
+    calculator.customCalculation(func);
+    expect(func).toBeCalled();
   });
+  test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달된 함수가 1, 2, 3인자를 받아 호출한다.", () => {
+    const func = jest.fn((x, y, z) => x + y + z);
+    calculator.customCalculation(func, 1, 2, 3);
+    expect(func).toBeCalledWith(1, 2, 3);
+  });
+  test("abs함수는 Math.abs를 호출한다.", () => {
+    const spy = jest.spyOn(Math, 'abs');
+    calculator.abs(0);
+    expect(spy).toBeCalledTimes(1);
+  });
+});
